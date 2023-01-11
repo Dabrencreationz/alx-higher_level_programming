4bdf09781cc4bc22399eb0272dc084bebac8f10e
Brenda <brenvaltessy@yahoo.com>
2023-01-10 19:13:40 -0800

    python i/o

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
+"""
+Read file function
+"""
 
 
 def read_file(filename=""):
+    """read a file
+    args:
+        filename: file to manipulate
+    return:
+        na
+    """
 
-    with open(filename, "r", encoding="UTF-8") as f:
-
-        print(f.read(), end="")
+    with open(filename, encoding="utf-8") as f:
+        for line in f.read():
+            print(line, end="")
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
+"""
+write method
+"""
 
 
-def number_of_lines(filename=""):
+def write_file(filename="", text=""):
+    """write to file
+    args:
+        filename: file to manipulate
+    text:
+        text: string to write with
+    return:
+        number of characters written
+    """
 
-    with open(filename, "r", encoding="UTF-8") as f:
-
-        return len(list(f))
+    with open(filename, mode="w", encoding="utf-8") as f:
+        if f.write(text):
+            return len(text)
