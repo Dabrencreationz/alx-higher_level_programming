d55e9bba9d60b3bf33dd54e7ae05e4a6dc873538
Brenda <brenvaltessy@yahoo.com>
2023-03-14 17:07:30 -0700

    javascript objects and scope closures

--- 0x13-javascript_objects_scopes_closures/7-occurences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurences.js
@@ -1,5 +1,11 @@
 #!/usr/bin/node
 
-const nbOccurences = (list, searchElement) => list.filter((element) => searchElement === element).length;
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
 
-module.exports = { nbOccurences };
+  for (let i = 0; i < list.length; i++) {
+    count = (list[i] === searchElement ? count + 1 : count);
+  }
+
+  return count;
+};
