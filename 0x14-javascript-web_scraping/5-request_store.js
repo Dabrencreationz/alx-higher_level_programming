dff25eea74213f90d82e799f29479e04eeabb4e9
Brenda <brenvaltessy@yahoo.com>
2023-04-25 13:29:58 -0700

    javascript web scraping

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,18 +1,4 @@
-i#!/usr/bin/node
+#!/usr/bin/node
+const fs = require('fs');
 const request = require('request');
-
-request(process.argv[2], function (err, response, body) {
-  if (err == null) {
-    const resp = {};
-    const json = JSON.parse(body);
-    for (let i = 0; i < json.length; i++) {
-      if (json[i].completed === true) {
-        if (resp[json[i].userId] === undefined) {
-          resp[json[i].userId] = 0;
-        }
-        resp[json[i].userId]++;
-      }
-    }
-    console.log(resp);
-  }
-});
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
